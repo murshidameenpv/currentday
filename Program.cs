bd6f7e5dd641371f8a40ce717d8b28590fd9ae2c
codewithmurshid <82975683+murshidameenpv@users.noreply.github.com>
2022-08-24 02:22:39 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;  
+  
+namespace DateTimeFormatInCSharpSample  
+{  
+    class Program  
+    {  
+        static void Main(string[] args)  
+        {  
+              
+            DateTime aDate = DateTime.Now;  
+              Console.WriteLine(aDate.ToString("MMMM/dddd"));  
+              
+            Console.ReadKey();  
+        }  
+    }  
+}  
+  
